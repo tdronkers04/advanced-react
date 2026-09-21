d47ed761cebdecdff183e6078ba41c8f560162b7
tdronkers04 <tim@dronkers.dev>
2026-09-21 11:49:49 -0600

    add scaffolding for first chapter

--- src/app/chapter/01/page.tsx
+++ src/app/chapter/01/page.tsx
@@ -1,3 +1,8 @@
 export default function Chapter01() {
-    return <div>Hello World</div>
+  return (
+    <div>
+      <h1 className='pb-1 text-xl'>Chapter 01</h1>
+      <h2 className='pb-2 text-lg'>Intro to Re-Renders</h2>
+    </div>
+  )
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,16 +3,10 @@ import Link from 'next/link'
 export default function Home() {
   return (
     <div className='p-6'>
-      <h1 className='py-3 text-xl'>Advanced React Notes and Examples</h1>
+      <h1 className='pb-3 text-xl'>Advanced React Notes and Examples</h1>
       <ul className='flex flex-col gap-2 px-2'>
         <li>
-          <Link href='/chapter/01'>Chapter 01</Link>
-        </li>
-        <li>
-          <Link href='/chapter/02'>Chapter 02</Link>
-        </li>
-        <li>
-          <Link href='/chapter/03'>Chapter 03</Link>
+          <Link href='/chapter/01'>Chapter 01: Intro to Re-Renders</Link>
         </li>
       </ul>
     </div>
