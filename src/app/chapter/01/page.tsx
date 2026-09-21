56686cffbbbf3133a27dad24b0a72309a5cbd02a
tdronkers04 <tim@dronkers.dev>
2026-09-21 11:42:33 -0600

    styling setup

---
+++ src/app/chapter/01/page.tsx
@@ -0,0 +1,3 @@
+export default function Chapter01() {
+    return <div>Hello World</div>
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,20 @@
+import Link from 'next/link'
+
 export default function Home() {
-    return <div className="">Test Commit</div>
+  return (
+    <div className='p-6'>
+      <h1 className='py-3 text-xl'>Advanced React Notes and Examples</h1>
+      <ul className='flex flex-col gap-2 px-2'>
+        <li>
+          <Link href='/chapter/01'>Chapter 01</Link>
+        </li>
+        <li>
+          <Link href='/chapter/02'>Chapter 02</Link>
+        </li>
+        <li>
+          <Link href='/chapter/03'>Chapter 03</Link>
+        </li>
+      </ul>
+    </div>
+  )
 }
