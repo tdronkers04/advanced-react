27fd7db7e0ee87b36b684acc694c0e0b10f481f1
tdronkers04 <tim@dronkers.dev>
2026-09-21 12:28:57 -0600

    attribution

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,13 +2,21 @@ import Link from 'next/link'
 
 export default function Home() {
   return (
-    <div className='p-6'>
-      <h1 className='pb-3 text-xl'>Advanced React Notes and Examples</h1>
-      <ul className='flex flex-col gap-2 px-2'>
+    <div className='flex min-h-screen flex-col p-6'>
+      <div className='pb-3'>
+        <h1 className='text-xl'>Advanced React by Nadia Makarevich</h1>
+        <h2 className='text-lg'>Notes and Examples</h2>
+      </div>
+      <ul className='flex flex-col gap-2 overflow-auto px-2'>
         <li>
           <Link href='/chapter/01'>Chapter 01: Intro to Re-Renders</Link>
         </li>
       </ul>
+      <div className='mt-auto italic underline'>
+        <Link href='https://www.advanced-react.com/' target='_blank'>
+          Buy Advanced React by Nadia Makarevich
+        </Link>
+      </div>
     </div>
   )
 }
